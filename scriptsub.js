6baf459c95993b050fe0c8549edd4fbdb8f83989
aashish dhanawat <ashishdhanawat111@gmail.com>
2026-02-20 23:58:17 +0530

    initial

--- scriptsub.js
+++ scriptsub.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.pricing-card').forEach(card => {
   card.addEventListener('mouseenter', () => {
-    card.style.boxShadow = "0 20px 40px rgba(0,0,0,0.05)";
+    card.style.boxShadow = "0 20px 40px rgba(0,0,0,0.1)";
     card.style.transition = "all 0.3s ease";
   });
   card.addEventListener('mouseleave', () => {
